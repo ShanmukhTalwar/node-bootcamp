b4021a8c69bf82cf67bf3fd0d6eed0ace3c0e9cd
shanmukh Talawar <shanmukhtalaward@gamil.com>
2023-07-02 17:17:29 +0530

    configered the styled componennt library

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
 import Landing from "./pages/Landing";
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: red;
+  color: white;
+  font-size: 1rem;
+`
+
+const ButtonSecond = styled.button`
+  background: red;
+  color: white;
+  font-size: 1rem;
+`
 
 function App() {
   return (
     <div>
+      <Button>Click ME</Button>
+      <ButtonSecond>Click ME</ButtonSecond>
       <h1>jobify</h1>
       <Landing/>
     </div>
