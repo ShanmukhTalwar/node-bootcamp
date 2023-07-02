5be0822c27a0f29922baa1427cf911f44147cc71
shanmukh Talawar <shanmukhtalaward@gamil.com>
2023-07-02 23:15:52 +0530

    added css to the Lander page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,10 +1,11 @@
 import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
+import styled from 'styled-components'
 
 const Landing = () => {
   return (
     <div>
-        <main>
+        <Wrapper>
             <nav>
                 <img src={logo} alt='jobify' className='logo'/>
             </nav>
@@ -12,9 +13,10 @@ const Landing = () => {
                 <div className="info">
                     <h1>job <span>Tracking</span> App</h1>
                     <p>
-                    Write the following inside the 'emmet.includeLanguages' tag. 
-                    Otherwise, paste the whole statement. 
-                    "emmet.includeLanguages"
+                    I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+                    bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+                    quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+                    narwhal.
                     </p>
                     <button className="btn btn-hero">
                         Login/Register
@@ -22,9 +24,46 @@ const Landing = () => {
                     <img src={main} alt='job hunt' className="img main-img"/>
                 </div>
             </div>
-        </main>
+        </Wrapper>
     </div>
   )
 }
 
+const Wrapper = styled.main`
+    nav{
+        width: var(--fluid-width);
+        max-width: var(--max-width);
+        margin: 0 auto;
+        height: var(--nav-height);
+        display: flex;
+        align-items: center;
+    }
+    .page{
+        min-height:calc(100vh -  var(--nav-height));
+        display: grid;
+        align-items: center;
+        margin-top: -3rem;
+    }
+    h1{
+        font-weight: 700;
+        span{
+            color: var(--primary-500);
+        }
+    }
+    p{
+        color: var(--grey-600);
+    }
+    .main-img{
+        display: none;
+    }
+    @media (min-width:992px) {
+        .page{
+            grid-template-columns: 1fr 1fr;
+            column-gap:3rem ;
+        }
+        .main-img{
+            display: block;
+        }
+    }
+`
 export default Landing
