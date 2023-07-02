decc0bc20ebca5be29a803eba89452e55e2e8f4d
shanmukh Talawar <shanmukhtalaward@gamil.com>
2023-07-02 17:01:47 +0530

    Landing Page created

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -4,7 +4,25 @@ import main from '../assets/images/main.svg'
 const Landing = () => {
   return (
     <div>
-      <h4>landing Page</h4>
+        <main>
+            <nav>
+                <img src={logo} alt='jobify' className='logo'/>
+            </nav>
+            <div className="container page">
+                <div className="info">
+                    <h1>job <span>Tracking</span> App</h1>
+                    <p>
+                    Write the following inside the 'emmet.includeLanguages' tag. 
+                    Otherwise, paste the whole statement. 
+                    "emmet.includeLanguages"
+                    </p>
+                    <button className="btn btn-hero">
+                        Login/Register
+                    </button>
+                    <img src={main} alt='job hunt' className="img main-img"/>
+                </div>
+            </div>
+        </main>
     </div>
   )
 }
