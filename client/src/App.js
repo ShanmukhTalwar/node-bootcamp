1d244244abd261baeb40cecff6c800dcb7924e46
shanmukh Talawar <shanmukhtalaward@gamil.com>
2023-07-09 23:56:02 +0530

    Test Component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Dashboard,Error,Landing,Register } from "./pages";
+import UseState from './test-components/useState';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Dashboard></Dashboard>}></Route>
-        <Route path='/register' element={<Register></Register>}></Route>
-        <Route path='/landing' element={<Landing></Landing>}></Route>
-        <Route path='*' element={<Error></Error>}></Route>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={<Dashboard></Dashboard>}></Route>
+    //     <Route path='/register' element={<Register></Register>}></Route>
+    //     <Route path='/landing' element={<Landing></Landing>}></Route>
+    //     <Route path='*' element={<Error></Error>}></Route>
+    //   </Routes>
+    // </BrowserRouter>
+    <UseState></UseState>
   );
 }
 
---
+++ client/src/test-components/useState.js
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+
+export default function UseState() {
+    const [isToggled, setIsToggled] = useState(false);
+
+    const handleToggle = () => {
+      setIsToggled(!isToggled);
+    };
+  
+    return (
+      <div>
+        <button onClick={handleToggle}>Toggle</button>
+        {isToggled && <p>Toggle is ON</p>}
+        {!isToggled && <p>Toggle is OFF</p>}
+      </div>
+    );
+  };
+  
